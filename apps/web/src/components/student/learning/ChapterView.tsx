202572bd994e18f7d4ed74a3146d68bd514a0526
BekaChkhiro <bekachkhirodze1@gmail.com>
2025-12-03 16:59:12 +0400

    new tab learn page

--- apps/web/src/components/student/learning/ChapterView.tsx
+++ apps/web/src/components/student/learning/ChapterView.tsx
@@ -8,8 +8,9 @@ import VideoPlayer from './VideoPlayer';
 import TheorySection from './TheorySection';
 import AssignmentSection from './AssignmentSection';
 import CommentsSection from './CommentsSection';
+import AttachmentsPreview from './AttachmentsPreview';
 
-type ActiveTab = 'video' | 'theory' | 'assignment' | 'quiz';
+type ActiveTab = 'video' | 'theory' | 'materials' | 'assignment' | 'quiz';
 
 interface ChapterViewProps {
   courseSlug: string;
@@ -183,6 +184,16 @@ export default function ChapterView({
       ),
     },
     {
+      id: 'materials',
+      label: 'მასალები',
+      available: true, // Always show materials tab
+      icon: (
+        <svg className="w-4 h-4" fill="currentColor" viewBox="0 0 20 20">
+          <path fillRule="evenodd" d="M4 4a2 2 0 00-2 2v8a2 2 0 002 2h12a2 2 0 002-2V8a2 2 0 00-2-2h-5L9 4H4zm7 5a1 1 0 00-2 0v1H8a1 1 0 000 2h1v1a1 1 0 002 0v-1h1a1 1 0 000-2h-1V9z" clipRule="evenodd" />
+        </svg>
+      ),
+    },
+    {
       id: 'assignment',
       label: 'Assignment',
       available: !!chapter.assignmentFile,
@@ -312,6 +323,15 @@ export default function ChapterView({
               queryClient.invalidateQueries({ queryKey: ['chapterForLearning', chapterId] });
             }}
           />
+        )}
+
+        {/* Materials Tab */}
+        {activeTab === 'materials' && (
+          <div className="p-6">
+            <div className="max-w-4xl mx-auto">
+              <AttachmentsPreview chapterId={chapterId} />
+            </div>
+          </div>
         )}
 
         {/* Assignment Tab */}
